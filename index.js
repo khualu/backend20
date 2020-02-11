bb1d418c7ffe02691ccbfb9d81f5a2211a57133b
Khualu <andrespint@gmail.com>
2020-02-11 14:59:01 +0100

    added an image for testing

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const camelCase = require('camelcase');
+// const camelCase = require('camelcase');
 const path = require('path');
-// console.log(camelCase('tortilla-de-patatas'));
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use('/static', express.static('public'));
+// app.use('/static', express.static('public'));
 
-app.get('*', function(req, res){
-    res.status(404).send('TEEEEEEEEEEEEEEEEST');
-  });
+// app.get('*', function(req, res){
+//     res.status(404).send('FOUTE LINK');
+//   });
 // WHY USE THIS UNDERNEATH
-// app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.listen(port, () => console.log(`Server is now listening on port ${port}`));
 
+// http://localhost:3000/static/index.html
+// http://localhost:3000/static/about.html
+// http://localhost:3000/static/images/animu.jpg
