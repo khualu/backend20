16b1226ed9bab91be812ae28be1b7457385ef107
Khualu <andrespint@gmail.com>
2020-02-26 13:30:53 +0100

    Added server functionality of POST. User is now able to write 3 favorite games which get saved in a JSON file. Next step is to render these games in EJS

--- server.js
+++ server.js
@@ -1,35 +1,59 @@
 // const camelCase = require('camelcase');
 const path = require('path');
 const ejs = require('ejs');
-
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// MIGHTY SERVER EXPRESS STATIC
-// app.use('/', express.static(path.join(__dirname, 'public')))
-//     .use('/audio', express.static(path.join(__dirname, '/audio')))
-//     .get('*', (req, res) => res.status(404).send("This page does not exist."))
-//     .listen(port, () => console.log(`Server is now listening on port ${port}`));
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+app.use(bodyParser.json());
 
 // EJS TEMPLATE SERVER
 app.set('view engine', 'ejs');
 app.use(express.static('src'));
-// index page
+
+// INDEX PAGE
 app.get('/', function(req, res) {
     res.render('pages/index.ejs');
 })
-
+// ABOUT PAGE
 app.get('/about', function(req, res) {
     res.render('pages/about.ejs');
 })
 
+// POST FAVORITE GAME
+app.post('/', function(req, res) {
+    // console.log((JSON.stringify(req.body)));
+    let testGame = req.body;
+    console.log(testGame);
+
+    saveGame(testGame, function(err) {
+        if(err) {
+            res.status(404).send('Game not send');
+            return;
+        } 
+        // res.end(JSON.stringify(req.body));
+        res.send("Favorite games are saved");
+    })
+})
+
+// CATCH REST REDIRECT TO HOME
 app.get('*', function(req, res) {
     res.redirect('/');
 })
 
+// WHICH PORT
 app.listen(port);
 console.log(`Server is listening to ${port}`);
+
+
+function saveGame(testGame, cb) {
+    fs.writeFile('./views/pages/game.json', JSON.stringify(testGame), cb);
+}
 
 // http://localhost:3000/static/index.html
 // http://localhost:3000/static/about.html
