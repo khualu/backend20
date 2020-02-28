8876e05a5b422468f2c21523b41315119db307db
Khualu <andrespint@gmail.com>
2020-02-28 09:34:42 +0100

    Commit for first feedback at CMD. User can post three favorite games into a json-file. This gets loaded into the homepage through EJS template. Next step will be to get the section that renders the top 3 games into a EJS partial

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-// const camelCase = require('camelcase');
 const path = require('path');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+
+
 const express = require('express');
 const app = express();
 const port = 3000;
+
+// IMPORT GAMES.JSON FILE
+const games = require('./views/pages/game.json');
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -18,7 +22,8 @@ app.use(express.static('src'));
 
 // INDEX PAGE
 app.get('/', function(req, res) {
-    res.render('pages/index.ejs');
+    // res.render('pages/index.ejs', games);
+    res.render('pages/index.ejs', games);
 })
 // ABOUT PAGE
 app.get('/about', function(req, res) {
@@ -36,8 +41,9 @@ app.post('/', function(req, res) {
             res.status(404).send('Game not send');
             return;
         } 
-        // res.end(JSON.stringify(req.body));
-        res.send("Favorite games are saved");
+        // console.log("Your games are saved");
+        // res.render('pages/index.ejs', games)
+        res.send("Your games are saved");
     })
 })
 
@@ -55,6 +61,13 @@ function saveGame(testGame, cb) {
     fs.writeFile('./views/pages/game.json', JSON.stringify(testGame), cb);
 }
 
+
+// let test = {
+//     people: [
+//         {name: 'dave'},
+//         {name: 'tortilla'}
+//     ]
+// };
 // http://localhost:3000/static/index.html
 // http://localhost:3000/static/about.html
 // http://localhost:3000/static/images/animu.jpg
